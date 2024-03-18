ac7b11c48af118c8d0c92434b2261f0b0c4bb88f
liluriel28 <a3521110073@alumno.uttehuacan.edu.mx>
2024-03-17 22:05:30 -0600

    cambio1

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,87 @@
+'use client'
+import Head from 'next/head';
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Home() {
+
+  const [posts, setPosts] = useState([]);
+  const apiEndPoint = 'https://jsonplaceholder.typicode.com/posts'
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const { data: res } = await axios.get(apiEndPoint);
+        setPosts(res);
+      }
+      catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    };
+    getPosts();
+  },
+    []
+  )
+
+  const addPost = async () => {
+    const post = { title: 'New Post', body: 'new' }
+    await axios.post(apiEndPoint, post);
+    setPosts([post, ...posts]);
+  }
+
+  const handleUpdate = async (post) => {
+    post.title = 'Updated title';
+    await axios.put(apiEndPoint + '/' + post.id);
+    const postClone = [...posts];
+    const index = postClone.indexOf(post);
+    postClone[index] = { ...post };
+    setPosts(postClone);
+  }
+
+  const handleDelete = async post => {
+    await axios.delete(apiEndPoint + '/' + post.id + post);
+    setPosts(posts.filter(p => p.id !== post.id));
+  }
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <h1 className={styles.h2}>Hay {posts.length} publicaciones </h1>
+          <button className={styles.agregar} onClick={addPost}>Agregar Post</button>
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Update</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {posts.map(post =>
+                <tr key={post.id}>
+                  <td>{post.title}</td>
+                  <td>
+                    <button className={styles.update} onClick={() => handleUpdate(post)}>Update</button>
+                  </td>
+                  <td>
+                    <button className={styles.delete} onClick={() => handleDelete(post)}>Delete</button>
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
         </div>
-      </div>
+      </main>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    </>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   );
 }
